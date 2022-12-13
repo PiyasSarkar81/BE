c2d4b548af184f91cbe5368354505c9323c51b17
PiyasSarkar81 <96674830+PiyasSarkar81@users.noreply.github.com>
2022-12-13 20:29:18 +0530

    Update prog02.c

--- DSA/Assignment2/prog02.c
+++ DSA/Assignment2/prog02.c
@@ -6,17 +6,21 @@ struct node{
     struct node* link;
 };
 
-struct node *addatlist(struct node *last, int data, int pos){
-    struct node *tmp,*p;
-    int i;
-    tmp = (struct node*)malloc(sizeof (struct node));
+struct node *addtoempty(struct node *last, int data)
+{
+    struct node *tmp;
+    tmp = (struct node *)malloc(sizeof(struct node));
     tmp -> info = data;
-    if(pos == 1)
-    {
-        last = tmp;
-        last -> link = last;
-        return last;
-    }
+    last = tmp;
+    last -> link = last;
+    return last;
+}
+
+struct node *addatend(struct node *last, int data)
+{
+    struct node* tmp;
+    tmp = (struct node*)malloc(sizeof(struct node));
+    tmp -> info = data;
     tmp -> link = last -> link;
     last -> link = tmp;
     last = tmp;
@@ -24,66 +28,40 @@ struct node *addatlist(struct node *last, int data, int pos){
 }
 
 struct node *create_list(struct node * last){
-    int n,i;
-    struct node *p = last;
-    printf("Enter the number of elements in the circular link list.\n");
+    int n,i,data;
+    printf("Enter the length of the Josephus promlem.\n");
     scanf("%d",&n);
     last = NULL;
     if(n==0) return last;
-    // printf("Enter the %d elements\n",n);
+    last = addtoempty(last,1);
 
-    for(i=1;i<=n;i++){
-        // int data;
-        // scanf("%d",&data);
-        last = addatlist(last,i,i);
+    for(i=2;i<=n;i++){
+        last = addatend(last,i);
     }
     return last;
 } 
 
-void display(struct node *last){
-    struct node *p;
-    if(last == NULL){
-        printf("List is empty\n");
-        return;
-    }
-    p= last -> link;
-    printf("List is.\n");
-    do{
-        printf("%d ",p->info);
-        p=p->link;
-    }while(p!=last->link);
-    printf("\n\n");
-}
-
-
-int count(struct node * last){
-    struct node * p;
-    p=last -> link;
-    int c =0;
-    do{
-        p=p->link;
-        c++;
-    }while(p!= last -> link);
-    return c;
-}
-
-void josephusCircle(struct node* last,int k){
-    struct node * p, *tmp;
-    p = last ;
-    while(count(last)>1){
-     for(int i=1;i<k;i++){
-            p=p->link;
+int survivor(struct node *last, int k)
+{
+    struct node *p, *q;
+    int i;
+ 
+    q = p = last -> link;
+    while (p->link != p)
+    {
+        for (i = 0; i < k - 1; i++)
+        {
+            q = p;
+            p = p->link;
         }
-        if(p->link == last){
-            last = p;
-        }
-        tmp = p->link;
-        p->link = tmp -> link;
-        free(tmp);
-        if(p-> link == p)
-        printf("Josephus Circle number : %d",p->info);
+        q->link = p->link;
+        printf("%d has been killed.\n", p->info);
+        free(p);
+        p = q->link;
     }
-
+    last = p;
+ 
+    return (p->info);
 }
 
 int main(){
@@ -92,6 +70,7 @@ int main(){
     printf("Count to choose next : ");
     int k;
     scanf("%d",&k);
-    josephusCircle(last,k);
+    
+    printf("survivor is : %d",survivor(last, k));
     return 0;
 }
